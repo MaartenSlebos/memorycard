977879878772c065ed72389dd952271db57943f1
MaartenSlebos <maarten.slebos@hotmail.com>
2025-04-18 18:49:36 +0200

    fetch balbasaur

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import './App.css'
+import { useState, useEffect } from 'react'; 
+
+async function fetchPokemon(num) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${num}`
+  const response = await fetch(url); 
+  const data = await response.json(); 
+  console.log(data); 
+  return data; 
+}
+
 function App() {
+  const [pokemon, setPokemon] = useState(null); 
+
+  useEffect(() => {
+    fetchPokemon(1).then(data => setPokemon(data)); 
+  }, []); 
+  console.log(pokemon); 
+
   return (
     <>
-      <h1>Memory Game</h1>
+      <h1>Pokemon</h1>
+      {pokemon ? <h2>{pokemon.name}</h2> : <h2>Loading...</h2>}
     </>
   )
 }
 
-export default App
+export default App; 
+
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+function Card(props) {
+    return (
+        <div>
+            <h1>Card</h1>
+            <h2>{props.name}</h2>
+        </div>
+    )
+}
+
+export default Card
+
+
